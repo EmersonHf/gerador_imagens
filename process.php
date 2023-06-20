b4bb5c5e9a6891ad3de3335752abb5c95000a052
Emerson <emersonfilho1@hotmail.com>
2023-06-20 00:58:45 -0300

    retorna 200 mas imagem não ta renderizando

--- process.php
+++ process.php
@@ -1,78 +1,91 @@
-
 <?php
 function addLogo($sourceImagePath, $logoImagePath, $outputImagePath)
 {
-    
-    // Create new Imagick objects
-    $anuncio = new Imagick($sourceImagePath);
-    $anuncioWidth = $anuncio->getImageWidth();
-    echo $anuncioWidth;
-    exit;
-    $logo = new Imagick($logoImagePath);
-    
-    // Get the dimensions of the source image and watermark
-    
-    $anuncioHeight = $anuncio->getImageHeight();
-    
-    $logokWidth = $logo->getImageWidth();
-    $logokHeight = $logo->getImageHeight();
+    try {
 
-    // Calculate the position to place the watermark at the bottom right corner
-    $positionX = $anuncioWidth - $logokWidth - 10;
-    $positionY = $anuncioHeight - $logokHeight - 10;
+        if (empty($logoPath)) {
+            throw new ImagickException('Diretório da logo não foi configurado.');
+        }
+        // Create new Imagick objects for source image and logo
+        $anuncio = new Imagick($sourceImagePath);
+        $logo = new Imagick($logoImagePath);
 
-    // Composite the watermark onto the source image
-    $anuncio->compositeImage($logo, Imagick::COMPOSITE_OVER, $positionX, $positionY);
 
-    // Set the image format to JPEG and quality to 90 (adjust as needed)
-    $anuncio->setImageFormat('jpeg');
-    $anuncio->setImageCompressionQuality(90);
+        $anuncioWidth = $anuncio->getImageWidth();
+        $anuncioHeight = $anuncio->getImageHeight();
 
-    // Save the modified image to the output path
-    $med = $anuncio->writeImage($outputImagePath);
-    if(!$med){
-        echo 'deu ruim';
+        $logoWidth = $logo->getImageWidth();
+        $logoHeight = $logo->getImageHeight();
 
-    }else{
-        echo 'deu';
+      // Calculate the position to place the logo at the bottom right corner
+      $positionX = $anuncioWidth - $logoWidth - 10;
+      $positionY = $anuncioHeight - $logoHeight - 10;
+
+        // Add watermark/logo to the source image
+        // Modify the code below according to your desired watermarking logic
+        $anuncio->compositeImage($logo, Imagick::COMPOSITE_OVER, $positionX, $positionY);
+  
+  
+        // Set the image format to JPEG and quality to 90 (adjust as needed)
+        $anuncio->setImageFormat('jpeg');
+        $anuncio->setImageCompressionQuality(90);
+
+        // Save the result image to the output path
+
+        $anuncio->writeImage($outputImagePath);
+
+        // Destroy the Imagick objects to free up memory
+        $anuncio->destroy();
+        $logo->destroy();
+    } catch (ImagickException $e) {
+        // Handle any exceptions that occur during the process
+        echo 'Error: ' . $e->getMessage();
     }
-    exit;
-    // Clear the memory
-    $anuncio->clear();
-    $anuncio->destroy();
-    
 }
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  
-    
-    // Retrieve the selected image and logo
-    $selectedImage = __DIR__.'/images/stamp.jpg';
-    $logo = $_FILES['logo'];
-    
-    // Process the image and add the watermark
-  
-    $outputImage = __DIR__.'/images/imagensGeradas.png';
+    // Check if a file was uploaded
+    if (isset($_FILES['logo']) && $_FILES['logo']['error'] === UPLOAD_ERR_OK) {
+        // Set the paths for the source image, logo, and output image
+        $selectedImage = $_POST['selectedImage'];
+        $logoTmpPath = $_FILES['logo']['tmp_name']; // Temporary path
+        $logoFileName = $_FILES['logo']['name']; // Original file name
+        $logoImagePath = __DIR__.'/images/uploads/logos/' . $logoFileName; 
 
-    // Call the function to add the watermark
-    addLogo($selectedImage, $logo, $outputImage);
-    echo $outputImage;
-    exit;
-    // Save the modified image to a file (modify this as per your requirement)
-    
-    // $image->writeImage($outputImage);
+        $outputImagePath = __DIR__.'/imagensGeradas/result_image.png'; 
+      
+        // echo $selectedImage;
+        // exit;
+        // echo "imagem selecionada(anuncio0x.png):", $selectedImage ;
+        // echo "<br><br>";
+        // echo "caminho temporario da logo:", $logoTmpPath;
+        // echo "<br><br>";    
+        // echo "imagem da logo:", $logoImagePath;
+        //     echo "<br><br>";    
+        echo "caminho de saida:", $outputImagePath;
+            echo "<br><br>";
+        // exit;
+        
+        // Move the uploaded logo file to the permanent location
+        move_uploaded_file($logoTmpPath, $logoImagePath);
+        
+        // Process the image and add the watermark/logo
+        addLogo($selectedImage, $logoImagePath, $outputImagePath);
+        
+        header('Content-Type: image/png');
+        header('Content-Length: ' . filesize($outputImagePath));
 
-    // Generate a unique ID for the modified image
-    $imageID = uniqid();
-
-    // Provide the link to download the modified image
-    $downloadLink = "download.php?image=$imageID";
-
-
-    // Return the modified image for the user to download
-    header('Content-Type: image/png');
-    header('Content-Disposition: filename="modified_image.png"');
-    readfile($outputImage);
-    exit;
-
+        
+        
+        // Display the result and provide a download link
+        
+        echo '<h1>Image Processing Result</h1>';
+        echo '<img src="' . $outputImagePath . '" alt="Result Image" width="400" height="400"><br><br>';
+        echo '<a href="' . $outputImagePath . '" download>Download Result Image</a>';
+    } else {
+        echo 'Error: Please select a logo image.';
+    }
+} else {
+    echo 'Error: Invalid request.';
 }
