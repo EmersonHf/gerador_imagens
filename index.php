3a9db5a5b763377fdad0d4d32f9b5fb9938695d7
emerson <emerson.higino@locarmais.com>
2023-06-22 17:36:24 -0300

    recurso do imagick adicionado a imagem do story

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Locarmais </title>
+   
+    <link rel="stylesheet" href="css/style.css">
     <style>
-        .image-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-           
-        }
-        img{
-            border-radius: 20px;
-        }
-        .image-item {
-            margin: 10px; 
-           
-           
-        }
-    </style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap');
+</style>
 </head>
 <body>
+    <header class="logo-bg">
+        <div class="container header-img">
+          <img class="nav-img" src="images/nav/LOGO.png" alt="logoLocarMais" srcset="">     
+        </div>
+    </div>
+    </header>
     <h1>Selecione uma imagem de divulgação</h1>
     <form action="process.php" method="POST" enctype="multipart/form-data">
         <div class="image-container">
 
             <div class="image-item">
-                <input type="radio" name="selectedImage" value="anuncio01.jpg" id="anuncio01">
+                <input type="radio" name="selectedImage" value="anuncio01.jpg" id="anuncio01" checked>
                 <label for="anuncio01">
                     <img src="/images/anuncio01.jpg" alt="anuncio01" width="200" height="200">
                 </label>
@@ -66,27 +63,27 @@
         <div class="image-container">
 
 <div class="image-item">
-    <input type="radio" name="selectedImage" value="storyanuncio01.jpg" id="storyanuncio01">
+    <input checked type="radio" name="selectedStoryImage" value="storyanuncio01.jpg" id="storyanuncio01">
     <label for="storyanuncio01">
-        <img src="/images/storyImages/storyanuncio01.jpg" alt="storyanuncio01" width="150" height="220">
+        <img src="/images/storyImages/storyanuncio01.jpg" alt="storyanuncio01" width="150" height="250">
     </label>
 </div>
 <div class="image-item">
-    <input type="radio" name="selectedImage" value="storyanuncio02.jpg" id="storyanuncio02">
+    <input type="radio" name="selectedStoryImage" value="storyanuncio02.jpg" id="storyanuncio02">
     <label for="storyanuncio02">
-        <img src="/images/storyImages/storyanuncio02.jpg" alt="storyanuncio02" width="150" height="220">
+        <img src="/images/storyImages/storyanuncio02.jpg" alt="storyanuncio02" width="150" height="250">
     </label>
 </div>
 <div class="image-item">
-    <input type="radio" name="selectedImage" value="storyanuncio03.jpg" id="storyanuncio03">
+    <input type="radio" name="selectedStoryImage" value="storyanuncio03.jpg" id="storyanuncio03">
     <label for="storyanuncio03">
-        <img src="/images/storyImages/storyanuncio03.jpg" alt="storyanuncio03" width="150" height="220">
+        <img src="/images/storyImages/storyanuncio03.jpg" alt="storyanuncio03" width="150" height="250">
     </label>
 </div>
 <div class="image-item">
-    <input type="radio" name="selectedImage" value="storyanuncio04.jpg" id="storyanuncio04">
+    <input type="radio" name="selectedStoryImage" value="storyanuncio04.jpg" id="storyanuncio04">
     <label for="storyanuncio04">
-        <img src="/images/storyImages/storyanuncio04.jpg" alt="storyanuncio04" width="150" height="220">
+        <img src="/images/storyImages/storyanuncio04.jpg" alt="storyanuncio04" width="150" height="250">
     </label>
 </div>
 
--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-function addLogo($selectedImage, $logoImagePath, $outputImagePath)
+function addLogo($selectedImage, $logoImagePath, $outputImagePath,$outputStoryImagePath)
 {
    
     try {
@@ -7,54 +7,100 @@ function addLogo($selectedImage, $logoImagePath, $outputImagePath)
         if (empty($logoImagePath)) {
             throw new ImagickException('Diretório da logo não foi configurado.');
         }
-        
-       
-        // Create new Imagick objects for source image and logo
-            $anuncio = new Imagick();
-            $selectedImage =  __DIR__.'/images/'.$_POST['selectedImage'];
-          
-            if (file_exists($selectedImage)) {
 
-                $anuncio->readImage($selectedImage);
-                $anuncioWidth = $anuncio->getImageWidth();
-                $anuncioHeight = $anuncio->getImageHeight();
+            if($_POST['selectedStoryImage'] && isset($_POST['selectedStoryImage']))
+            {
+                $story = new Imagick();
+                $selectedStoryImage = __DIR__.'/images/storyImages/'.$_POST['selectedStoryImage'];
 
-            } else {
-                throw new ImagickException('Imagem selecionada não existe.');
-            }
 
-        $logo = new Imagick($logoImagePath);
+                if (file_exists($selectedStoryImage)) {
 
-        // $logoWidth = $logo->getImageWidth();
-        // $logoHeight = $logo->getImageHeight();
+                    $story->readImage($selectedStoryImage);
+                    $storyWidth = $story->getImageWidth();
+                    $storyHeight = $story->getImageHeight();
     
-        $logoWidth = 200;
-        $logoHeight = 200;
-        
-        // Resize the logo image
-        $logo->scaleImage($logoWidth, $logoHeight, true);
+                } else {
+                    throw new ImagickException('Imagem selecionada não existe.');
+                }
 
+                $logo = new Imagick($logoImagePath);
 
-      // Calculate the position to place the logo in the bottom right corner
-      $positionX = 170; // Left margin
-      $positionY = $anuncioHeight - $logoHeight - 11; // Bottom margin
-      
-       
- 
-        $anuncio->compositeImage($logo, Imagick::COMPOSITE_OVER, $positionX, $positionY);
+                $logoWidth = 200;
+                $logoHeight = 200;
+                
+                
+                // Resize the logo image
+                $logo->scaleImage($logoWidth, $logoHeight, true);
+                // Calculate the position to place the logo in the bottom left corner
+                 $positionX = 0; // Left margin
+                 $positionY = $storyHeight - $logoHeight - 246; // Bottom margin
+
+                 $story->compositeImage($logo, Imagick::COMPOSITE_OVER, $positionX, $positionY);
   
-  
-        // Set the image format to JPEG and quality to 90 (adjust as needed)
-        $anuncio->setImageFormat('png');
-        $anuncio->setImageCompressionQuality(90);
-        
-        // Save the result image to the output path
+                 // Set the image format to png and quality to 90 (adjust as needed)
+                 $story->setImageFormat('png');
+                 $story->setImageCompressionQuality(90);
+                 
+                 // Save the result image to the output path
+         
+                 $story->writeImage($outputStoryImagePath);
 
-        $anuncio->writeImage($outputImagePath);
-       
-        // Destroy the Imagick objects to free up memory
-        $anuncio->destroy();
-        $logo->destroy();
+                 header('Content-disposition: inline');
+
+                 // Destroy the Imagick objects to free up memory
+                 $story->destroy();
+                 $logo->destroy();
+                 
+            } 
+            
+                // Create new Imagick objects for source image and logo
+                $anuncio = new Imagick();
+                $selectedImage =  __DIR__.'/images/'.$_POST['selectedImage'];
+  
+                if (file_exists($selectedImage)) {
+
+                    $anuncio->readImage($selectedImage);
+                    $anuncioWidth = $anuncio->getImageWidth();
+                    $anuncioHeight = $anuncio->getImageHeight();
+
+                } else {
+                    throw new ImagickException('Imagem selecionada não existe.');
+                }
+
+                $logo = new Imagick($logoImagePath);
+
+                // $logoWidth = $logo->getImageWidth();
+                // $logoHeight = $logo->getImageHeight();
+            
+
+
+
+                $logoWidth = 200;
+                $logoHeight = 200;
+                
+                // Resize the logo image
+                $logo->scaleImage($logoWidth, $logoHeight, true);
+
+
+                // Calculate the position to place the logo in the bottom right corner
+                $positionX = 170; // Left margin
+                $positionY = $anuncioHeight - $logoHeight - 11; // Bottom margin
+                
+        
+    
+                $anuncio->compositeImage($logo, Imagick::COMPOSITE_OVER, $positionX, $positionY);
+    
+    
+                // Set the image format to JPEG and quality to 90 (adjust as needed)
+                $anuncio->setImageFormat('png');
+                $anuncio->setImageCompressionQuality(90);
+                $anuncio->writeImage($outputImagePath);
+        
+                // Destroy the Imagick objects to free up memory
+                $anuncio->destroy();
+                $logo->destroy();
+            
     } catch (ImagickException $e) {
         // Handle any exceptions that occur during the process
         echo 'Error: ' . $e->getMessage();
@@ -66,23 +112,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if a file was uploaded
     if (isset($_FILES['logo']) && $_FILES['logo']['error'] === UPLOAD_ERR_OK) {
         
-        $selectedImage = __DIR__.'/images/'.$_POST['selectedImage'];
+        $selectedImage = '/images/'.$_POST['selectedImage'];
+        $selectedStoryImage = '/images/storyImages/'.$_POST['selectedStoryImage'];
         $logoTmpPath = $_FILES['logo']['tmp_name']; // caminho temporario
         $logoFileName = $_FILES['logo']['name']; // nome do arquivo original
         $logoImagePath = __DIR__.'/images/uploads/logos/' . $logoFileName; 
 
         $outputImagePath = __DIR__.'/images/imagensGeradas/result_image.png'; 
+        $outputStoryImagePath = __DIR__.'/images/imagensGeradas/story/result_story_image.png'; 
       
     //     echo $selectedImage;
        
-    //     echo "imagem selecionada:", $selectedImage;
+    //     echo "saida imagem selecionada:", $outputImagePath;
     //    echo "<br><br>";
-    //     // exit;
+       
+    //    echo "saida imagem selecionada para story:", $outputStoryImagePath;
+    //    echo "<br><br>";
+    //     exit;
     //     echo "caminho temporario da logo:", $logoTmpPath;
     //     echo "<br><br>";    
     //     echo "imagem da logo:", $logoImagePath;
     //         echo "<br><br>";    
-    //     echo "caminho de saida:", $outputImagePath;
+    //     echo "caminho de saida da imagem:", $outputImagePath;
+    //         echo "<br><br>";
+    //         echo "caminho de saida do story:", $outputStoryImagePath;
     //         echo "<br><br>";
     //     exit;
         
@@ -90,18 +143,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         move_uploaded_file($logoTmpPath, $logoImagePath);
         
         // Process the image and add the watermark/logo
-        addLogo($selectedImage, $logoImagePath, $outputImagePath);
+        addLogo($selectedImage, $logoImagePath, $outputImagePath, $outputStoryImagePath);
         
       
         header('Content-disposition: inline');
        
         
-        // Display the result and provide a download link
         
-        echo '<h1>Resultado :</h1>';
+
+        if($_POST['selectedStoryImage'] && isset($_POST['selectedStoryImage']))
+        {
+            header('Content-disposition: inline');
+               
+        echo '<h1>Resultado Imagem de Story :</h1>';
+        echo '<img src="data:image/png;base64,' . base64_encode(file_get_contents($outputStoryImagePath)) . '" alt="Result Image" width="200" height="300"><br><br>';
+
+        echo '<a href="' . $outputStoryImagePath . '" download>Download</a>';
+      
+        }
+            // Display the result and provide a download link
+        
+        echo '<h1>Resultado Imagem :</h1>';
         echo '<img src="data:image/png;base64,' . base64_encode(file_get_contents($outputImagePath)) . '" alt="Result Image" width="400" height="400"><br><br>';
 
         echo '<a href="' . $outputImagePath . '" download>Download</a>';
+        
+        exit;
     } else {
         echo 'Error: Por favor, selecione uma imagem para logo.';
     }
